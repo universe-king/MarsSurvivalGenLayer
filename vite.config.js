1e169b7e44221615f14ef69599cc8f9f612051ec
universe-king <alirezadosti74@gmail.com>
2026-09-11 13:02:18 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,18 @@
-export default { server: { port: 3000, open: true } }
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        leaderboard: resolve(__dirname, 'leaderboard.html')
+      }
+    }
+  }
+})
